5f9c8133833ce804a56201e2170e22a74aa73707
viveknegi <vivek.negi@intrics.in>
2024-08-29 22:20:56 +0530

    initial commit

---
+++ Spherum/Spherum/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Spherum;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Spherum/Spherum/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Spherum;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
